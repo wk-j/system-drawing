ada0406f5d92d429a32d00a1f170d65ad30786c0
wk-j <somnuk.wk@gmail.com>
2018-09-10 17:44:40 +0700

    Initial commit

---
+++ src/SystemDrawing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing.Imaging;
+using Newtonsoft.Json;
+
+namespace SystemDrawing {
+    class MyData {
+        public ImageFormat ImageFormat { set; get; }
+        public int Value { set; get; }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var json = @"{
+                ""imageFormat"": 0,
+                ""value"": 1000
+            }
+            ";
+
+            // var obj = JsonConvert.DeserializeObject<MyData>(json);
+            var data = new MyData {
+                ImageFormat = ImageFormat.Bmp,
+                Value = 200
+            };
+
+            var newJson = JsonConvert.SerializeObject(data);
+
+            Console.WriteLine(newJson);
+
+            var newObject = JsonConvert.DeserializeObject<MyData>(newJson);
+            Console.WriteLine(newObject.ImageFormat == ImageFormat.Bmp);
+            Console.WriteLine(newObject.ImageFormat.Equals(ImageFormat.Bmp));
+            Console.WriteLine(newObject.ImageFormat);
+        }
+    }
+}
